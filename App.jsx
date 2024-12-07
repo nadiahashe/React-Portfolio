fd390d62f69b28d36703da2f8a90d5a8cb91b001
Nadia Hashemi <nadiahashemi@Nadias-MacBook-Pro-2.local>
2024-12-07 13:37:23 -0500

    package.json

---
+++ App.jsx
@@ -0,0 +1,20 @@
+// Bringing in the required import from 'react-router-dom'
+import { Outlet } from 'react-router-dom';
+import Nav from './components/NavTabs';
+import Footer from './components/Footer';
+import './App.css';
+
+function App() {
+  // The Outlet component will conditionally swap between the different pages according to the URL
+  return (
+    <>
+      <Nav />
+      <main >
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
